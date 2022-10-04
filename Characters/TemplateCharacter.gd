67325f5554f43cd9764c46d2dc3fb1aecb4020f8
Andrew G <vardantexcode@gmail.com>
2022-10-04 13:17:39 -0400

    Init

---
+++ Characters/TemplateCharacter.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+class_name Template_Character
+
+const SPEED = 10
+const MAX_SPEED = 100
+const FRICTION = .75
+
+
+
+func _ready() -> void:
+	pass
+
+
+
---
+++ Player.gd
@@ -0,0 +1,5 @@
+extends Template_Character
+
+
+func _ready() -> void:
+	pass
