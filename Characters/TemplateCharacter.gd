b4a7fcbd70297c660d463c74f35cfddce8ede57a
Andrew G <vardantexcode@gmail.com>
2022-10-04 16:46:20 -0400

    Movement and Cursor Tracking

--- Characters/TemplateCharacter.gd
+++ Characters/TemplateCharacter.gd
@@ -2,8 +2,8 @@ extends KinematicBody2D
 class_name Template_Character
 
 const SPEED = 10
-const MAX_SPEED = 100
-const FRICTION = .75
+const MAX_SPEED = 500
+const FRICTION = .25
 
 
 
--- Player.gd
+++ Player.gd
@@ -1,5 +1,34 @@
 extends Template_Character
 
+var motion = Vector2()
 
 func _ready() -> void:
 	pass
+
+func _physics_process(delta: float) -> void:
+	update_movement()
+	move_and_slide(motion)
+	
+	
+
+func update_movement():
+	
+	look_at(get_global_mouse_position())
+	
+	if Input.is_action_pressed("WASD_DOWN") and not Input.is_action_pressed("WASD_UP"):
+		#Use the clamp tool (vector, min value, max value)
+		motion.y = clamp(motion.y + SPEED, 0, MAX_SPEED)
+	elif Input.is_action_pressed("WASD_UP") and not Input.is_action_pressed("WASD_DOWN"):
+		motion.y = clamp(motion.y - SPEED, -MAX_SPEED, 0)
+	else: 
+		motion.y = lerp(motion.y, 0, FRICTION)
+	
+	if Input.is_action_pressed("WASD_LEFT") and not Input.is_action_pressed("WASD_RIGHT"):
+		#Use the clamp tool (vector, min value, max value)
+		motion.x = clamp(motion.x - SPEED, -MAX_SPEED, 0)
+	elif Input.is_action_pressed("WASD_RIGHT") and not Input.is_action_pressed("WASD_LEFT"):
+		motion.x = clamp(motion.x + SPEED,  MAX_SPEED, 0)
+	else: 
+		motion.x = lerp(motion.x, 0, FRICTION)
+	
+	
